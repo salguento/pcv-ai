b0f4f2bcff97dc4849ef1ededbe13265b66e1d38
Salguento <salguento@gmail.com>
2024-09-30 16:34:08 -0300

    add nuxt-image config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,17 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/content',
     '@nuxt/image'
-  ]
+  ],
+  image: {
+    // The screen sizes predefined by `@nuxt/image`:
+    screens: {
+      'xs': 320,
+      'sm': 640,
+      'md': 768,
+      'lg': 1024,
+      'xl': 1280,
+      'xxl': 1536,
+      '2xl': 1536
+    },
+  }
 })
