92c6010838c147b0cab3940aa7f8cb4a590d39d0
Salguento <salguento@gmail.com>
2024-10-01 22:39:47 -0300

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-slate-50 text-center dark:bg-slate-900">
+    <div
+      class="bg-black/5 p-4 text-center text-surface dark:text-white font-semibold"
+    >
+      2024 👾
+      <a
+        href="https://salguento.xyz"
+        class="dark:hover:text-blue-500 hover:text-blue-600"
+        >Humberto Salguento</a
+      >
+    </div>
+  </footer>
+</template>
