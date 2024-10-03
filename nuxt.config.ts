b079dc6b661f9b832991f15866f382f728a51b5e
Salguento <salguento@gmail.com>
2024-10-02 22:38:28 -0300

    ssr to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode',
-    '@nuxt/content',
-    '@nuxt/image',
-    'nuxt-headlessui'
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+    "@nuxt/image",
+    "nuxt-headlessui",
   ],
   image: {
     // The screen sizes predefined by `@nuxt/image`:
     screens: {
-      'xs': 320,
-      'sm': 640,
-      'md': 768,
-      'lg': 1024,
-      'xl': 1280,
-      'xxl': 1536,
-      '2xl': 1536
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      "2xl": 1536,
     },
-  }
-})
+  },
+  ssr: false,
+});
